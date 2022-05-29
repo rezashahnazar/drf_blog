ad826800443bf17a56ca1851db3a7b778103fabb
Reza <reza.shahnazar@gmail.com>
2022-05-29 17:28:10 +0430

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import hi
+from .views import createPost
 
 urlpatterns = [
-    path('', hi)
+    path('post', createPost),
+    # path('posts/', ),
+    # path('post/<int:id>',)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Post
 
 
-@api_view()
-def hi(request):
-    return Response({"message": "hi"})
+@api_view(['POST'])
+def createPost(request):
+    if request.method == 'POST':
+        result = Post.objects.all()
+        return Response(result)
